0c2580f83c742058ac12e79a4b7454e9c4f3a3c9
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-03 16:12:41 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "brown", "hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "rowdy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "brown", "dull", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "F", "black", "bold", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "M", "tan", "clever", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "M", "red", "greedy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "F", "tan", "timid", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 5, "F", "brown", "reckless", 0);
