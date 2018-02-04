573d45235ecb0f96c8a01a2f685170d70b81dcfe
allinthemind66 <r.nilon92@gmail.com>
2018-02-04 17:13:36 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Johnny", 22, "male", "white", "cool", 0),
+(2, "Mark", 22, "male", "white", "relaxed", 1),
+(3, "Denny", 16, "male", "white", "stupid", 1),
+(4, "Lisa", 24, "female", "white", "evil", 1),
+(5, "Claudette", 65, "female", "white", "smart", 1),
+(6, "Michelle", 24, "female", "white", "inquisitive", 1),
+(7, "Chris-R", 26, "male", "white", "agressive", 0),
+(8, NULL, 26, "male", "white", "angry", 1);
