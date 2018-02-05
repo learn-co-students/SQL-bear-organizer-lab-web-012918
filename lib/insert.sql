e8c24fd3d291c413f474fbcb61fe312b5b7b4631
johnvoyage <jmyork762@gmail.com>
2018-02-05 11:08:23 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Mr. Chocolate", NULL, NULL, NULL, NULL, NULL),
+("Rowdy", NULL, NULL, NULL, NULL, NULL),
+("Tabitha", NULL, NULL, NULL, NULL, NULL),
+("Sergeant Brown", NULL, NULL, NULL, NULL, NULL),
+("Melissa", NULL, NULL, NULL, NULL, NULL),
+("Grinch", NULL, NULL, NULL, NULL, NULL),
+("Wendy", NULL, NULL, NULL, NULL, NULL),
+(NULL, NULL, NULL, NULL, NULL, NULL);
