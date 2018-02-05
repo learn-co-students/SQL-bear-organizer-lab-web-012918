097a989393c28253772441d1b644d4f5fa32f2a5
github username <github email address>
2018-02-05 16:46:58 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,25 +1,25 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE gender IS 'F';"
+  "SELECT name, age FROM bears WHERE gender IS 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT bears.name FROM bears ORDER BY name;"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name, bears.age FROM bears WHERE alive is '1' ORDER BY age ASC;"
+  "SELECT name, age FROM bears WHERE alive is '1' ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT bears.color, COUNT(color) FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY color DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT bears.id FROM bears WHERE name IS 'nil';"
+  "SELECT * FROM bears WHERE name IS null;"
 end
